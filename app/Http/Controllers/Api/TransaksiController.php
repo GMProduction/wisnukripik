4911be16b519af7afe73dc4dc77e86ecda03f1d2
pradana <pradanamahendra@gmail.com>
2022-02-22 15:06:01 +0700

    a

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -176,7 +176,7 @@ class TransaksiController extends Controller
         if($request->image->getClientOriginalName()){
             $file = str_replace(' ', '', $request->image->getClientOriginalName());
             $fileName = date('mYdHs').rand(1,100).'_'.$file;
-            $request->image->move(public_path('../../public_html/wisnukripik/dist/transfer'), $fileName);
+            $request->image->move('/home/u7082880/public_html/wisnukripik/dist/transfer', $fileName);
         } else{
             return $this->error('Gagal Memuat Data');
         }
